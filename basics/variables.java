070afef791b1216e692a4d63e73dc9c382651e3a
devaanshi chaturvedi <114324947+devaanshi15@users.noreply.github.com>
2025-01-02 23:16:51 +0530

    vaiables

---
+++ basics/variables.java
@@ -0,0 +1,26 @@
+package basics;
+
+public class variables {
+
+    class A{
+        int a = 10;    //a - instance variable
+        static int b=20; //b - static variable
+        void add(){
+            int c = 30;     //c - local variable
+            int d=a+b+c;
+            System.out.println("Sum: "+d);
+        }
+
+        void mul(){
+            int e = 30,d;
+            d=a*b*e;            //we cant use c here
+            System.out.println("Product: "+d); 
+        }
+
+
+        void display(){
+            System.out.println("A: "+a);
+        }
+    }
+    
+}
